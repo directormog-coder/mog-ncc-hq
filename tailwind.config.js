7cad9e58ebb5c1fa18c5f8bb6573b72ab34c192b
directormog-coder <director.mog@gmail.com>
2026-03-07 20:35:11 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+        noir: '#050505',
+      },
+    },
+  },
+  plugins: [],
+}
